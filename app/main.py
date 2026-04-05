501c40b9a79fdcee1c4e3c4f6692c41f8f9cacfc
Leticia Bi <leticiabi@LeticiadeMacBook-Air.local>
2026-04-05 20:01:08 +0200

    fix: allow all origins

--- app/main.py
+++ app/main.py
@@ -11,9 +11,17 @@ app = FastAPI(
     version="1.0.0"
 )
 
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["http://localhost:5173", "https://tubular-klepon-2ec933.netlify.app"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://tubular-klepon-2ec933.netlify.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
