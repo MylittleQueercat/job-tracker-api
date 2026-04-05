9607baba1da7ed3b6c4f93bec64b1abae778434a
Leticia Bi <leticiabi@LeticiadeMacBook-Air.local>
2026-04-05 19:33:12 +0200

    vercel domaine

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://*.vercel.app"],
+    allow_origins=["http://localhost:5173", "https://job-tracker-lwhg7j4t2-mylittle-queercat.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
